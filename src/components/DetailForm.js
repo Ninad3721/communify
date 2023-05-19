75d32a690194a0320c0be62721d544eaf7952451
Ninad3721 <ninadtrivedi25@gmail.com>
2023-05-19 23:37:27 +0530

    Mongodb added

--- src/components/DetailForm.js
+++ src/components/DetailForm.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import TextField from '@mui/material/TextField';
 import { FormControl, InputLabel, Input } from '@mui/material';
+import Button from '@mui/material/Button';
+import axios from 'axios';
 
 function DetailForm() {
+
+    const handleSubmit = async () => {
+        try {
+            await axios.post('http://localhost:5000/details', {
+                dob: document.getElementById('dob').value,
+                user: document.getElementById('username').value
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+
+
+    }
     return (
         <div>
-            <TextField type="date">Date of  Birth</TextField>
-            <FormControl>
-                <InputLabel htmlFor="my-input">Username</InputLabel>
-                <Input id="my-input" aria-describedby="my-helper-text" />
-            </FormControl>
+            <form>
+                <TextField type="date" id='dob'>Date of  Birth</TextField>
+                <FormControl>
+                    <InputLabel htmlFor="my-input">Username</InputLabel>
+                    <Input id="username" aria-describedby="my-helper-text" />
+                </FormControl>
+                <FormControl>
+                </FormControl>
+                <Button variant="contained" color="success" onClick={handleSubmit}>
+                    Success
+                </Button>
+            </form>
+
         </div>
     )
 }
