c4b8cef2e0492213fa2bb2524df9a7f6d45e04c0
Ninad3721 <ninadtrivedi25@gmail.com>
2023-06-24 16:42:36 +0530

    Signup added

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,11 @@
-import { TextField, InputAdornment, Icon, } from "@mui/material";
+import { TextField } from "@mui/material";
 import Button from '@mui/material/Button';
 import { useState } from 'react'
 import axios from 'axios'
-import { IconButton } from '@mui/material';
 import technologist from '../images/apple_technologist.png';
+import postbox from "../images/apple_postbox.png"
+import oldkey from "../images/apple-old-key.png"
+import background from "../images/login-background.png"
 
 function Signup() {
     const [email, setEmail] = useState("")
@@ -18,7 +20,7 @@ function Signup() {
     }
     return (
         <>
-            <div className="relative rounded-xl bg-white w-full h-[882px] overflow-hidden text-left text-5xl text-black font-inter">
+            <div className="absolute left-52 rounded-xl bg-white w-[1024px] h-[720px] overflow-hidden text-left text-5xl text-black font-inter" >
                 <TextField
                     className="[border:none] bg-[transparent] absolute top-[342px] left-[373px]"
                     sx={{ width: 310 }}
@@ -26,65 +28,55 @@ function Signup() {
                     variant="filled"
                     type="text"
                     name="Email"
-                    label="Email"
-                    placeholder="Email"
+                    placeholder="gavinbelson@hooli.com"
                     size="medium"
                     margin="none"
                     onChange={(event) => { setEmail(event.target.value) }}
                 />
                 <TextField
-                    className="[border:none] bg-[transparent] absolute top-[446px] left-[373px]"
+                    className="[border:none] bg-[transparent] absolute top-[460px] left-[60px]"
                     sx={{ width: 310 }}
                     color="primary"
                     variant="filled"
-                    type="text"
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <IconButton aria-label="toggle password visibility">
-                                    <Icon>visibility</Icon>
-                                </IconButton>
-                            </InputAdornment>
-                        ),
-                    }}
+                    type="password"
                     name="Password"
-                    label="Set Password"
-                    placeholder="Set Password"
+                    label="Password"
+                    placeholder="Password"
                     size="medium"
                     margin="none"
                     onChange={(event) => { setPassword(event.target.value) }}
                 />
-                <div className="absolute top-[302px] left-[410px] font-light inline-block w-[318px] h-7">
+                <div className="absolute top-[302px] left-[410px] font-light inline-block w-[318px] h-7 text-xl" >
                     Email
                 </div>
                 <img
                     className="absolute top-[303px] left-[380px] w-[30px] h-[29px] object-cover"
                     alt="mail box"
-                    src="/image-14@2x.png"
+                    src={postbox}
                 />
                 <h4 className="m-0 absolute top-[405px] left-[415px]" />
+                <div className="absolute top-[415px] left-[410px] font-light inline-block w-[318px] h-7 text-xl" >
+                    Password
+                </div>
                 <img
                     className="absolute top-[405px] left-[380px] w-[30px] h-[30px] object-cover"
-                    alt="Key"
-                    src="/image-13@2x.png"
+                    src={oldkey}
                 />
-                <a href="/" className="[text-decoration:none] absolute top-[529px] left-[436px] font-light text-[inherit] inline-block w-[265px] h-[51px]">
-                    Already have an account !!!!!!
+                <a href="/" className="[text-decoration:none] absolute top-[600px] left-[460px] font-light text-[inherit] inline-block w-[265px] h-[51px] text-xl text-blue-600 underline">
+                    or Login !!!!!!
                 </a>
-                <h2 className="m-0 absolute top-[204px] left-[380px] text-[48px] font-normal font-motley-forces text-mediumspringgreen inline-block w-[341px] h-[73px] mix-blend-multiply">
+                <h2 className="m-0 absolute top-[204px] left-[380px] text-[48px] font-normal font-motley-forces text-mediumspringgreen inline-block w-[341px] h-[73px] mix-blend-multiply " style={{
+                    fontFamily: "Motley Forces", color: "#00D88E"
+                }}>
                     Communify
                 </h2>
                 <img
-                    className="absolute top-[209px] left-[625px] w-9 h-[41px]"
-                    src="https://em-content.zobj.net/thumbs/120/apple/354/technologist-medium-skin-tone_1f9d1-1f3fd-200d-1f4bb.png"
+                    className="absolute top-[209px] left-[625px] w-9 h-[41px] object-cover"
+                    src={technologist}
                 />
-                <img
-                    className="absolute top-[0px] left-[0px] w-[1224px] h-[938px] object-cover opacity-[0.1]"
-                    alt="background"
-                    src="https://em-content.zobj.net/thumbs/120/apple/354/technologist-medium-skin-tone_1f9d1-1f3fd-200d-1f4bb.png"
-                />
-                <Button variant="contained" onClick={handelSubmit()}>Submit</Button>
-            </div>
+
+                <Button sx={{ width: 310 }} className="absolute top-[529px] left-[-250px] text-blue underline bg-green-500" variant="contained" onClick={handelSubmit()}>Submit</Button>
+            </div >
         </>
     )
 }
